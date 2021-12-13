e608d779dff8edda823658a2066547def4c363f9
Rajsingh92 <rajsinghrajpoot263@gmail.com>
2021-12-13 12:33:37 +0530

    13-12-21

--- MASTER SQL/interchange the adjacent student names.sql
+++ MASTER SQL/interchange the adjacent student names.sql
@@ -1,10 +1,8 @@
--- Query 6:
-
+/*
 From the students table, write a SQL query to interchange the adjacent student names.
 
 Note: If there are no adjacent student then the student name should stay the same.
 
---Table Structure:
 
 drop table students;
 create table students
@@ -19,11 +17,15 @@ insert into students values
 (4, 'Stewart'),
 (5, 'Robin');
 
-select * from students;
+*/
 
---Solution:
 
-select id,student_name,
-case when id%2 <> 0 then lead(student_name,1,student_name) over(order by id)
-when id%2 = 0 then lag(student_name) over(order by id) end as new_student_name
+select 
+	id,
+	student_name,
+	case 
+		when id%2 <> 0 then lead(student_name,1,student_name) over(order by id)
+		when id%2 = 0 then lag(student_name) over(order by id) 
+	end as new_student_name
 from students;
+
